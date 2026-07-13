1ac9672bd9f43a95ff1f3b78dd3b1fbb46cd8bd7
Joyaljose371 <joyaljosepallivathukkal371@gmail.com>
2026-07-13 20:04:49 +0530

    feat: added public folder with PWA icons inside psych-prep-app

--- psych-prep-app/src/main.jsx
+++ psych-prep-app/src/main.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-
+// Add this line to handle automatic service worker updates
+import { registerSW } from 'virtual:pwa-register'
+registerSW({ immediate: true })
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
